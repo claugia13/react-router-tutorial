a7082badb0f7ef9a92b4170b62d063fc6180993d
claugia13 <ac.delima13@gmail.com>
2021-03-04 18:45:20 -0700

    import compoents and react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import Nav from './Nav';
+import About from './About';
+import Shop from './Shop';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <h1>App</h1>
+      <Nav />
+      <Route path="/about" component={About} />
+      <Route path="/shop" component={Shop} />
     </div>
+    </Router>
   );
 }
 
